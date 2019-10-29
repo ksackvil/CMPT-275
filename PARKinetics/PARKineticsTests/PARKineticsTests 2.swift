0bf73f89ded89fdc4ff22ece31d9c01119b300a2
Armaan Bandali <armaanbandali@Armaans-MacBook-Pro.local>
2019-10-28 17:39:04 -0700

    Added background for Finger Twister

---
+++ PARKinetics/PARKineticsTests/PARKineticsTests 2.swift
@@ -0,0 +1,35 @@
+//
+//  PARKineticsTests.swift
+//  PARKineticsTests
+//
+//  Created by Kai Sackville-Hii on 2019-10-04.
+//  Copyright © 2019 Kai Sackville-Hii. All rights reserved.
+//
+
+import XCTest
+@testable import PARKinetics
+
+class PARKineticsTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
