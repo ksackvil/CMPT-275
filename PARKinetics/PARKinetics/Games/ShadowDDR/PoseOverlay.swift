246365671727b7eb1921c392765aeb65f4fa63ad
ksackvil <sackvillehiik@gmail.com>
2019-11-20 20:32:47 -0800

    pose verification added, game loop missing. Support only for T pose with right arm.

--- PARKinetics/PARKinetics/Games/ShadowDDR/PoseOverlay.swift
+++ PARKinetics/PARKinetics/Games/ShadowDDR/PoseOverlay.swift
@@ -13,10 +13,10 @@ class PoseOverlay : UIView {
     let line = CAShapeLayer()
     let linePath = UIBezierPath()
     
-    public var sourX: CGFloat = 5.0
-    public var sourY: CGFloat = 5.0
-    public var destX: CGFloat = 50.0
-    public var destY: CGFloat = 50.0
+    public var sourX: CGFloat = 0.0
+    public var sourY: CGFloat = 0.0
+    public var destX: CGFloat = 0.5
+    public var destY: CGFloat = 0.5
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -32,13 +32,12 @@ class PoseOverlay : UIView {
     }
     
     override func draw(_ rect: CGRect) {
-        print(sourX)
         if let context = UIGraphicsGetCurrentContext() {
             context.setStrokeColor(UIColor.blue.cgColor)
-            context.setLineWidth(3)
+            context.setLineWidth(20)
             context.beginPath()
-            context.move(to: CGPoint(x: sourX ,y: sourY)) // This would be oldX, oldY
-            context.addLine(to: CGPoint(x: destX, y: destY)) // This would be newX, newY
+            context.move(to: CGPoint(x: sourX*self.frame.width ,y: sourY*self.frame.height)) // This would be oldX, oldY
+            context.addLine(to: CGPoint(x: destX*self.frame.width, y: destY*self.frame.height)) // This would be newX, newY
             context.strokePath()
         }
     }
--- PARKinetics/PARKinetics/Games/ShadowDDR/ShadowDDRVC.swift
+++ PARKinetics/PARKinetics/Games/ShadowDDR/ShadowDDRVC.swift
@@ -5,6 +5,43 @@
 //  Created by Kai Sackville-Hii on 2019-11-15.
 //  Copyright © 2019 Kai Sackville-Hii. All rights reserved.
 //
+// Apendix A:
+// below are the extracted keypoints from the given image as defined above, these
+// vectors should be used for validating our positions. if you where to print foundRightLeg after
+// correctly exctracting the pose then the output would look something like this...
+/*
+ 
+ [Keypoint(id: 12, position: (0.46718414098836103, 0.5267753033732557), score: 0.880, part: HumanSkeleton), Keypoint(id: 14, position: (0.46964614215064143, 0.6969218024451065), score: 0.641, part: HumanSkeleton), Keypoint(id: 16, position: (0.5093932652751759, 0.807005468914259), score: 0.536, part: HumanSkeleton)]
+ 
+ */
+// Each element in the array is a Keypoint object, each element has an id which lets you
+// know which keypoint you it is, its position, and a accuracy score. The position is normalized
+// from 0 to 1, with (0, 0) = (x, y) = the top left of an up oriented image.
+// Use the table below to map an id with its keypoint. See this link for more info
+// https://docs.fritz.ai/develop/vision/pose-estimation/ios.html
+/*
+ 
+ id | keypoint
+ ---|---------
+ 0  | nose
+ 1  | left eye
+ 2  | right eye
+ 3  | left ear
+ 4  | right ear
+ 5  | left shoulder
+ 6  | right shoulder
+ 7  | left elbow
+ 8  | right elbow
+ 9  | left wrist
+ 10 | right wrist
+ 11 | left hip
+ 12 | right hip
+ 13 | left knee
+ 14 | right knee
+ 15 | left ankle
+ 16 | right ankle
+ 
+ */
 
 import UIKit
 import Foundation
@@ -12,7 +49,13 @@ import Fritz
 
 class ShadowDDRVC: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     var previewView: UIImageView!
-    var overlay: PoseOverlay!
+    var checkMark: UIImageView!
+//    var overlay1: PoseOverlay!
+//    var overlay2: PoseOverlay!
+    var messageDisplayed = false
+    var poseItr: Int = 0;
+    var minPoseThreshold: Double { return 0.4 }
+    private var captureSession: AVCaptureSession!    // our video session handeler
     
     // below are all the keypoints of the HumanSkeleton Object whitch we would like to
     // extract from the image. To make it easier on ourselves we divide the body into sections
@@ -23,42 +66,7 @@ class ShadowDDRVC: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegat
     private let rightLegParts: [HumanSkeleton] = [.rightAnkle, .rightKnee, .rightHip]
     private let faceParts: [HumanSkeleton] = [.rightEar, .leftEar, .rightEye, .leftEye, .nose]
     
-    // Below are the extracted keypoints from the given image as defined above, these
-    // vectors should be used for validating our positions. if you where to print foundRightLeg after
-    // correctly exctracting the pose then the output would look something like this...
-    /*
-     
-     [Keypoint(id: 12, position: (0.46718414098836103, 0.5267753033732557), score: 0.880, part: HumanSkeleton), Keypoint(id: 14, position: (0.46964614215064143, 0.6969218024451065), score: 0.641, part: HumanSkeleton), Keypoint(id: 16, position: (0.5093932652751759, 0.807005468914259), score: 0.536, part: HumanSkeleton)]
-     
-     */
-    // Each element in the array is a Keypoint object, each element has an id which lets you
-    // know which keypoint you it is, its position, and a accuracy score. The position is normalized
-    // from 0 to 1, with (0, 0) = (x, y) = the top left of an up oriented image.
-    // Use the table below to map an id with its keypoint. See this link for more info
-    // https://docs.fritz.ai/develop/vision/pose-estimation/ios.html
-    /*
-     
-     id | keypoint
-     ---|---------
-     0  | nose
-     1  | left eye
-     2  | right eye
-     3  | left ear
-     4  | right ear
-     5  | left shoulder
-     6  | right shoulder
-     7  | left elbow
-     8  | right elbow
-     9  | left wrist
-     10 | right wrist
-     11 | left hip
-     12 | right hip
-     13 | left knee
-     14 | right knee
-     15 | left ankle
-     16 | right ankle
-     
-     */
+    // See Apendix A
     private var foundLeftArm: [Keypoint] = [] as! [Keypoint<HumanSkeleton>]
     private var foundRightArm: [Keypoint] = [] as! [Keypoint<HumanSkeleton>]
     private var foundLeftLeg: [Keypoint] = [] as! [Keypoint<HumanSkeleton>]
@@ -67,27 +75,9 @@ class ShadowDDRVC: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegat
     
     lazy var poseModel = FritzVisionHumanPoseModelFast()
     lazy var poseSmoother = PoseSmoother<OneEuroPointFilter, HumanSkeleton>()
-
-    // our video session handeler
-    private lazy var captureSession: AVCaptureSession = {
-      let session = AVCaptureSession()
-
-      guard
-        let frontCamera = AVCaptureDevice.default(
-            .builtInWideAngleCamera,
-          for: .video,
-          position: .front),
-        let input = try? AVCaptureDeviceInput(device: frontCamera)
-        else { return session }
-      session.addInput(input)
-
-      session.sessionPreset = .photo
     
-      return session
-    }()
 
-    var minPoseThreshold: Double { return 0.4 }
-    
+    // MARK: Overides
     // DES: Hides toolbar
     // PRE: View loaded from main menu segue
     // POST: Toolbar hidden
@@ -99,28 +89,48 @@ class ShadowDDRVC: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegat
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Add preview View as a subview
+        // previewView which hosues the pose estimations
         previewView = UIImageView(frame: view.bounds)
         previewView.contentMode = .scaleAspectFill
-    
-        // !*!* comment out this line when using videofeed
-//        testWithImage()
+        view.addSubview(previewView)
         
-        // !*!* uncomment below to enable video feed
+        // View for checkmark
+        checkMark = UIImageView(frame: view.bounds)
+        checkMark.image = UIImage(named:"correct.png")
+        checkMark.contentMode = .center
+        checkMark.alpha = 0
+        view.addSubview(checkMark)
+        view.bringSubviewToFront(checkMark) // ensures view is loaded in front of others
         
+        // our video session handeler
+        captureSession = {
+          let session = AVCaptureSession()
+
+          guard
+            let frontCamera = AVCaptureDevice.default(
+                .builtInWideAngleCamera,
+              for: .video,
+              position: .front),
+            let input = try? AVCaptureDeviceInput(device: frontCamera)
+            else { return session }
+          session.addInput(input)
+
+          session.sessionPreset = .photo
+        
+          return session
+        }()
+        
+        // Video session setup
         let videoOutput = AVCaptureVideoDataOutput()
         videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA as UInt32]
         videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyQueue"))
         self.captureSession.addOutput(videoOutput)
         self.captureSession.startRunning()
- 
-        
     }
     
     // DES: Sets up our view with subviews
     override func viewWillLayoutSubviews() {
       super.viewWillLayoutSubviews()
-
       previewView.frame = view.bounds
     }
 
@@ -130,20 +140,10 @@ class ShadowDDRVC: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegat
       // Dispose of any resources that can be recreated.
     }
     
-    // DES: This will display the output image with the pose estimations if any
-    // PRE: FritzVisionImage is given
-    // POST: Image is rendered into view
-    func displayInputImage(_ image: FritzVisionImage) {
-        guard let rotated = image.rotate() else { return }
-        
-        let image = UIImage(pixelBuffer: rotated)
-    
-        DispatchQueue.main.async {
-            self.previewView.image = image
-        }
-    }
-    
+    // MARK: Helper Fuctions
     // DES: takes the keypoints and formats into our body section vectors
+    // PRE: system was able to estimate pose in poseResult
+    // POST: each body vector will be updated accordingly
     func formatKeypoints(poseResult: FritzVisionPoseResult<HumanSkeleton>) {
         var tempLeftArm: [Keypoint] = [] as! [Keypoint<HumanSkeleton>]
         var tempRightArm: [Keypoint] = [] as! [Keypoint<HumanSkeleton>]
@@ -180,20 +180,51 @@ class ShadowDDRVC: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegat
         foundLeftLeg = tempLeftLeg
         foundRightLeg = tempRightLeg
         foundFace = tempFace
-        
-//        print("face")
-//        print(foundFace)
-//        print("leftArm")
-//        print(foundLeftArm)
-//        print("rightArm")
-//        print(foundRightArm)
-//        print("leftLeg")
-//        print(foundLeftLeg)
-//        print("rightLeg")
-//        print(foundRightLeg)
     }
     
+    // DES: Handles animation for flashing checkmark on successful pose
+    func flashCheckMark() {
+        UIView.animate(withDuration: 1, delay: 0.0 , animations: {
+            self.checkMark.alpha = 1
+        }, completion: {_ in
+            UIView.animate(withDuration: 1.2, delay: 0.0 , animations: {
+                  self.checkMark.alpha = 0
+            })
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                self.messageDisplayed = false
+            }
+        })
+    }
+    
+    // DES: main function for verifiying our expected poses
+    // PRE: A set of poses must be predefined, each case will have an id (0 - inf) this function
+    //      will verifiy based on the current poseItr (id).
+    // POST: if pose mathces current expected pose, func returns true
+    func verifyPose() -> Bool {
+        var verified = false
+        
+        switch self.poseItr {
+        case 0:
+            let rWristAboveShoulder = abs(Float(foundLeftArm[2].position.y) - Float(foundLeftArm[0].position.y)) < 0.1
+            let rWristLeftOfShoulder = Float(foundLeftArm[2].position.x) > Float(foundLeftArm[0].position.x)
+            
+            verified = rWristAboveShoulder && rWristLeftOfShoulder && messageDisplayed == false
+            
+        default:
+            verified = false
+        }
+        
+        return(verified)
+    }
+    
+    // MARK: Output Video Handlers
+    // DES: Will display the pose skeleton results created by our model
+    // PRE: poseResult must be non nil, should have exctracted pose
+    // POST: Image with pose skeleton will be renderd into view.
     func displayPoseEst(_ image: FritzVisionImage,_ poseResult: Pose<HumanSkeleton>) {
+        let success = verifyPose()
+        
         guard let newImg = image.draw(pose: poseResult) else {
             displayInputImage(image)
             return
@@ -201,25 +232,27 @@ class ShadowDDRVC: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegat
              
         DispatchQueue.main.async {
             self.previewView.image = newImg
+            
+            if success {
+                self.messageDisplayed = true
+                self.flashCheckMark()
+            }
+//            self.overlay1.reloadData()
+//            self.overlay2.reloadData()
         }
-
     }
     
-    func drawLineOnImage(size: CGSize, image: UIImage, from: CGPoint, to: CGPoint) -> UIImage {
-        UIGraphicsBeginImageContext(size)
-        image.draw(at: CGPoint.zero)
-        guard let context = UIGraphicsGetCurrentContext() else { return UIImage() }
-        context.setLineWidth(1.0)
-        context.setStrokeColor(UIColor.blue.cgColor)
-        let startPoint = CGPoint(x: from.x, y: from.y)
-        let endPoint = CGPoint(x: to.x, y: to.y)
-        context.move(to: startPoint)
-        context.addLine(to: endPoint)
-        context.strokePath()
-
-        guard let resultImage = UIGraphicsGetImageFromCurrentImageContext() else { return UIImage() }
-        UIGraphicsEndImageContext()
-        return resultImage
+    // DES: This will display the output image with the pose estimations if any
+    // PRE: FritzVisionImage is given
+    // POST: Image is rendered into view
+    func displayInputImage(_ image: FritzVisionImage) {
+        guard let rotated = image.rotate() else { return }
+        
+        let image = UIImage(pixelBuffer: rotated)
+    
+        DispatchQueue.main.async {
+            self.previewView.image = image
+        }
     }
     
     // DES: This is the main function which handles the pose estimation of a video input,
@@ -249,11 +282,6 @@ class ShadowDDRVC: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegat
         // Uncomment to use pose smoothing to smoothe output of model.
         // Will increase lag of pose a bit.
         // pose = poseSmoother.smoothe(pose)
-//
-//        guard let poseResult = image.draw(pose: pose) else {
-//            displayInputImage(image)
-//            return
-//        }
 
         formatKeypoints(poseResult: result)
         displayPoseEst(image, pose)
