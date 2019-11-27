5eb1e338775e3f17e15a7d9d2f1359e0b7667671
negar <nbagheri@sfu.ca>
2019-11-27 13:48:41 -0800

    New UI for levels

---
+++ PARKinetics/PARKinetics/Games/FingerTwister/DifficultyLevelVC.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  PARKinetics
+//
+//  Created by Negar Hariri on 2019-11-27.
+//  Copyright © 2019 Kai Sackville-Hii. All rights reserved.
+//
+
+
+
+import Foundation
+//
+//  MenuVC.swift
+//
+//
+//  Created by Negar Hariri on 2019-11-23.
+//
+
+import UIKit
+
+class DifficultyLevelVC: UIViewController {
+
+     
+    @IBAction func Level(_ sender: Any) {
+    }
+    
+    
+    @IBAction func LevelTwo(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+           super.viewDidLoad()
+    }
+    
+}
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+
